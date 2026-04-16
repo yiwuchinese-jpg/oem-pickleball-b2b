4dbec8beb3a1203b945fc82173ae007662e02ec7
Justin <ifanultra@gmail.com>
2026-04-16 16:17:53 +0800

    fix(api): add api index page to provide valid HTML to mock endpoints against 404 bots

---
+++ src/app/api/page.tsx
@@ -0,0 +1,12 @@
+export default function ApiDiscoveryPage() {
+  return (
+    <html>
+      <head>
+        <link rel="https://api.w.org/" href="https://pickleoem.com/api/wp-json/" />
+      </head>
+      <body>
+        <h1>Evolution 301 API Endpoint</h1>
+      </body>
+    </html>
+  );
+}
